41a6171608be0cdd3d4ad339e6d6ce86cd9f2d03
Jo Denby <jgdenby@uchicago.edu>
2018-03-04 13:14:44 -0600

    added model specification

--- hawksite/hawk_tracker/nn_model.py
+++ hawksite/hawk_tracker/nn_model.py
@@ -6,27 +6,36 @@ import numpy as np
 
 import sklearn
 import sklearn.neural_network
+import sklearn.naive_bayes
+import sklearn.tree
+import sklearn.ensemble
 
 from sklearn.model_selection import train_test_split
 
 
-def predict(new_text):
+def predict(model, new_text):
 	'''
-	Takes a new release text and, using a neural network model trained
+	Takes a model specification and new release text and, 
+	using the classifier specified, trains
 	on the tf-idf scores for all press releases since February 1994,
-	predicts whether the new text heralds an increase in the federal
+	predicting whether the new text heralds an increase in the federal
 	interest rate.
 
 	Inputs:
+		model (str): a string specifying the classifier desired
 		new_text (str): a string representation of the new release text
 
 	Outputs:
-		prediction (str): 
+		prediction (boolean) 
 	'''
-	# reads local csv with all releases from Feb 1994, the month & year
-	# of release, the numerical change in interest rate associated
-	# with that month, and a boolean expression indicating whether
-	# the interest rate increased
+
+	# model_strings = ['Neural Networks', 'Decision Tree', 'Bagging (Decision Tree)', 'Naive Bayes' ]
+
+	# reads local csv with all releases from Feb 1994 ['release_text'], 
+	# the month ['month'] & year ['year'] of release, 
+	# the change in interest rate associated with that month ['Change'],
+	# and a boolean expression indicating whether
+	# the interest rate increased ['increase']
 	releaserates_df = pd.read_csv('allreleaserates.csv', index_col=0)
 
 
@@ -38,9 +47,24 @@ def predict(new_text):
 	TFVects = TFVectorizer.fit_transform(releaserates_df['release_text'])
 	releaserates_df['vect'] = [np.array(v).flatten() for v in TFVects.todense()]
 
-	# instantiate and fit neural network model
-	clf_nn = sklearn.neural_network.MLPClassifier()
-	clf_nn.fit(np.stack(releaserates_df['vect'], axis=0), releaserates_df['increase'])
+	# instantiate model specified
+	if model == "Neural Networks":
+		clf = sklearn.neural_network.MLPClassifier()
+	
+	elif model == 'Decision Tree':
+		clf = sklearn.tree.DecisionTreeClassifier(max_depth=10)
+	
+	elif model == 'Bagging (Decision Tree)':
+		tree = sklearn.tree.DecisionTreeClassifier(max_depth=10) 
+		clf = sklearn.ensemble.BaggingClassifier(tree, n_estimators=100,
+			max_samples=0.8) 
+	
+	elif model == 'Naive Bayes':
+		clf = sklearn.naive_bayes.GaussianNB()
+
+
+	# fit model
+	clf.fit(np.stack(releaserates_df['vect'], axis=0), releaserates_df['increase'])
 
 	# compute tf-idf for new_text
 	TFVects_test = TFVectorizer.transform([new_text])
