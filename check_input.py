a46fa3fae41a35b16174f4928ed453cea05c7ce2
Natasha Mathur <natashamathur@uchicago.edu>
2018-03-04 17:49:01 -0600

    checks input for length, english, key word

---
+++ check_input.py
@@ -0,0 +1,31 @@
+
+
+from sklearn.model_selection import train_test_split
+
+def check(new_text, thresh = .25):
+    outcome = []
+
+    outcome.append(len(new_text) > 200)
+
+    d = enchant.Dict('en_US')
+
+    word_list = new_text.split(' ')
+    non_en = 0
+
+    for w in word_listL
+        if not d.check(w):
+            non_en += 1
+
+    outcome.append(non_en/len(word_list) <= thresh)
+
+    if "econ" not in new_text:
+        outcome.append(False)
+    else:
+        True
+
+    if False in outcome:
+        return False
+    else:
+        return True
+
+    
