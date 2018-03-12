2caaeec17fd1b1c73bb9efb2e58f2ac9402447fa
Natasha Mathur <natashamathur@uchicago.edu>
2018-03-12 16:19:36 -0500

    commented

--- hawksite/hawk_tracker/forms.py
+++ hawksite/hawk_tracker/forms.py
@@ -1,11 +1,6 @@
 from django import forms
 from .models import Query, Answer
 
-NEURAL_NETWORKS = 'NN'
-DECISION_TREE = 'DT'
-BAGGING = 'BAG'
-NAIVE_BAYES = 'NB'
-
 MODEL_CHOICES = [ ('HELP','Choose A Prediction Model!' ),
     ('NN', 'Neural Networks'),
     ('DT', 'Decision Tree'),
@@ -13,17 +8,19 @@ MODEL_CHOICES = [ ('HELP','Choose A Prediction Model!' ),
     ('NB', 'Naive Bayes')]
 
 class QueryForm(forms.ModelForm):
+    '''
+    Creates a form object that will be rendered to the user in order 
+    to submit a query to our model. 
+    '''
     query_text = forms.CharField(max_length=5000,
     widget = forms.Textarea(attrs={'cols': 110, 'rows': 20}), \
     initial="Add statement here...", label = '')
     query_method = forms.ChoiceField(widget=forms.Select(),
                               required=True, choices = MODEL_CHOICES,label = '')
 
-
-
-    # An inline class to provide additional information on the form.
+    # The models.py class that will be associated with this form. 
+    
     class Meta:
-        # Provide an association between the ModelForm and a model
         model = Query
         fields = ("query_text", "query_method")
         
