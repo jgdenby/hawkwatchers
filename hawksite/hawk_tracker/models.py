e250ccf604961d676fbd74bda15cbe6392339bca
Natasha Mathur <natashamathur@uchicago.edu>
2018-03-12 16:24:03 -0500

    commented

--- hawksite/hawk_tracker/models.py
+++ hawksite/hawk_tracker/models.py
@@ -3,16 +3,22 @@ from django.db import models
 from django.utils import timezone
 
 class Query(models.Model):
+    '''
+    This is used to create a Query object whose text can be processed by the models
+    as it has all the necessary attributes to run the model and create the display.  
+    '''
     query_text = models.CharField(max_length=1200)
     query_date = models.DateTimeField(default=timezone.now, blank=True)
     query_method = models.CharField(max_length=1200, default = 'NN')
     query_answer = models.CharField(max_length=1200, default = 'NEUTRAL')
 
-
     def __str__(self):
         return self.query_text
 
 class Answer(models.Model):
+    '''
+    This is used to create an Answer object that will be linked to a Query object. 
+    '''
     query_answer = models.OneToOneField(Query, on_delete=models.CASCADE, primary_key=True,) # an answer for a query
     answer_text = models.CharField(max_length=200) # hawkish, dovish
     answer_num = models.CharField(max_length=20) #boolean associated to the nltk model
@@ -21,6 +27,11 @@ class Answer(models.Model):
         return self.answer_text
 
 class Statement(models.Model):
+    '''
+    Contains information related to a particular Federal Reserve press release. 
+    The attributes are those which we want to display on the website.
+    These are directly added by the administrator only, not by a user. 
+    '''
     statement_title = models.CharField(max_length=1200, default ="Or find out what Hawkwatchers thinks about the latest Fed Statement!")
     statement_header = models.CharField(max_length=1200)
     statement_text = models.CharField(max_length=3200)
