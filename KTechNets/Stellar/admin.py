419f5b5209e9c2556a7fc7d09158bca8e4e148b1
ElenaBadilloG <elena.badillo.g@gmail.com>
2023-05-31 08:16:29 -0600

    prevcommit

--- KTechNets/Stellar/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-
-from .models import ArtChoice, ArtPath
-
-admin.site.register(ArtChoice)
-admin.site.register(ArtPath)
--- KTechNets/Stellar/templates/Stellar/results.php
+++
@@ -1,8 +0,0 @@
-<html>
-<body>
-
-Welcome <?php echo $_POST["query"]; ?><br>
-According to this statement, Hawkwatchers thinks that in its next scheduled meeting, the Fed will...; >
-
-</body>
-</html>
