ba2a14a999d1f8a4ed7dbc9b77e1063219076ddd
ElenaBadilloG <elena.badillo.g@gmail.com>
2023-06-02 10:49:29 -0600

    2nd

--- hawksite/hawk_tracker/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-
-from .models import Query, Answer, Statement
-
-admin.site.register(Query)
-admin.site.register(Answer)
-admin.site.register(Statement)
--- hawksite/hawk_tracker/apps.py
+++
@@ -1,4 +0,0 @@
-from django.apps import AppConfig
-
-class HawkTrackerConfig(AppConfig):
-    name = 'hawk_tracker'
